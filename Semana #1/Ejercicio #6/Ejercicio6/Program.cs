5bfc603fdc3599dc21a05db6eeca2b443ffbbaba
William <wgarciag@uamv.edu.ni>
2025-08-29 22:14:30 -0600

    Ejercicio #6

---
+++ Semana #1/Ejercicio #6/Ejercicio6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Ingrese el volumen promedio de leche por vaca (en litros): ");
+double litrosPorVaca = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ingrese el número total de vacas ordeñadas: ");
+int numeroVacas = Convert.ToInt32(Console.ReadLine());
+
+double litrosTotales = litrosPorVaca * numeroVacas;
+double baldes = litrosTotales / 20;
+
+Console.WriteLine("La cantidad total de baldes de leche producida es: " + baldes);
