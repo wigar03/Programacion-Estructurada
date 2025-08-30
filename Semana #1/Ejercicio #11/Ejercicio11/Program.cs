1959e8e18eb1b0ac6067889a8f66d3d4c0966406
William <wgarciag@uamv.edu.ni>
2025-08-29 22:25:34 -0600

    Ejercicio11

---
+++ Semana #1/Ejercicio #11/Ejercicio11/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Bienvenido a SwitchTarea");
+Console.WriteLine("Favor de ingresar su nota para poder verificar su calificación");
+Console.Write("Nota:");
+
+int nota = Convert.ToInt32(Console.ReadLine());
+
+switch (nota)
+{
+    case >= 90:
+        Console.WriteLine("Excelente");
+        break;
+
+    case >= 80:
+        Console.WriteLine("Bien hecho");
+        break;
+
+    case >= 70:
+        Console.WriteLine("Pasaste");
+        break;
+
+    case >= 60:
+        Console.WriteLine("Derecho a rescate");
+        break;
+
+    default:
+        Console.WriteLine("Reprobado");
+        break;
+}
