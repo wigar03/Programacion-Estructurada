b0050fad8b4e79495788adff87ab5d32837eef06
William <wgarciag@uamv.edu.ni>
2025-08-29 22:05:57 -0600

    Ejercicio #2

---
+++ Semana #1/Ejercicio #2/Ejercicio2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Ingrese la longitud del terreno en varas: ");
+double longitud = Convert.ToDouble(Console.ReadLine());
+
+// Utilizamos un ancho fijo de 5 varas
+double ancho = 5;
+
+// Hacemos el cálculo del área en varas²
+double areaVaras = longitud * ancho;
+
+// Convertimos el área a manzanas
+double areaManzanas = areaVaras / 1428.8;
+
+Console.WriteLine("El área total del terreno es: " + areaManzanas + " manzanas");
