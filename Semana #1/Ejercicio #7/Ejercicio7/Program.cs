f1230818aef08b73a8fb0296733fdc17e66a10b7
William <wgarciag@uamv.edu.ni>
2025-08-29 22:17:37 -0600

    Ejercicio #7

---
+++ Semana #1/Ejercicio #7/Ejercicio7/Program.cs
@@ -0,0 +1,14 @@
+﻿int num;
+
+Console.WriteLine("Programa para saber si un numéro es positivo o negativo");
+Console.WriteLine("Introduce un número");
+num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 0)
+{
+    Console.WriteLine("El número es positivo");
+}
+else
+{
+    Console.WriteLine("El número es negativo");
+}
