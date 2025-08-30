b51aa2c2566b77a6064bb5061544baa3a0e5110a
William <wgarciag@uamv.edu.ni>
2025-08-29 22:22:03 -0600

    Ejercicio #9

---
+++ Semana #1/Ejercicio #9/Ejercicio9/Program.cs
@@ -0,0 +1,16 @@
+﻿int i, sumapar = 0, sumaimpar = 0;
+for (i = 1; i < 200; i++)
+{
+    if (i % 2 == 0)
+    {
+        sumapar += i;
+    }
+    else
+    {
+        sumaimpar += i;
+    }
+}
+
+Console.WriteLine($"La suma de los números pares es: {sumapar}");
+Console.WriteLine($"La suma de los números impares es: {sumaimpar}");
+Console.WriteLine("Fin del programa");
