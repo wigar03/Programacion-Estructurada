0b969cd4d3de416d89a83c05077c8cc8e5e9cd88
William <wgarciag@uamv.edu.ni>
2025-08-29 22:02:09 -0600

    Ejercicio #1

---
+++ Semana #1/Ejercicio #1/Ejercicio1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ingresa un año para saber si es bisiesto o no:");
+Console.Write("Año: ");
+int año = Convert.ToInt32(Console.ReadLine());
+
+if ((año % 4 == 0 && año % 100 != 0) || (año % 400 == 0))
+{
+    Console.WriteLine($"{año} es un año bisiesto.");
+}
+else
+{
+    Console.WriteLine($"{año} no es un año bisiesto.");
+}
