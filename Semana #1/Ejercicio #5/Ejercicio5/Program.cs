f5e58d5b8f2263233ff66fdfc408f5bd6735f508
William <wgarciag@uamv.edu.ni>
2025-08-29 22:12:24 -0600

    Ejercicio #5

---
+++ Semana #1/Ejercicio #5/Ejercicio5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ingrese la longitud del tanque en metros: ");
+double longitud = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ingrese el ancho del tanque en metros: ");
+double ancho = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ingrese la altura del tanque en metros: ");
+double altura = Convert.ToDouble(Console.ReadLine());
+
+double volumen = longitud * ancho * altura;
+
+Console.WriteLine("El volumen total de agua en el tanque es: " + volumen + " m³");
