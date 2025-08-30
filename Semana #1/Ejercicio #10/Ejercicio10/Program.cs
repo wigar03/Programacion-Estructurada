a6f6cb4f00fe1495e1568cab350ed9fe0da1fde2
William <wgarciag@uamv.edu.ni>
2025-08-29 22:23:45 -0600

    Ejercicio #10

---
+++ Semana #1/Ejercicio #10/Ejercicio10/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Bienvenido a LeerNum");
+Console.WriteLine("Favor de ingresar una serie de numeros para determinar el mayor y el menor");
+int numero, mayor = int.MinValue, menor = int.MaxValue;
+Console.WriteLine("Para finalizar ingrese -99");
+
+do
+{
+    Console.Write("Digite un número: ");
+    numero = Convert.ToInt32(Console.ReadLine());
+
+    if (numero != -99)
+    {
+        if (numero > mayor)
+        {
+            mayor = numero;
+        }
+        if (numero < menor)
+        {
+            menor = numero;
+        }
+    }
+} while (numero != -99);
+
+Console.WriteLine($"El número mayor es: {mayor} y el numero menor es: {menor}");
+Console.WriteLine("Fin del programa");
