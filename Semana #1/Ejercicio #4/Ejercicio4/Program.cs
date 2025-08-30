113924f03168043c1f790b616b3e9c1e4b01c40f
William <wgarciag@uamv.edu.ni>
2025-08-29 22:10:43 -0600

    Ejercicio #4

---
+++ Semana #1/Ejercicio #4/Ejercicio4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Ingrese la velocidad promedio en km/h: ");
+double velocidad = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ingrese el tiempo recorrido en horas: ");
+double tiempo = Convert.ToDouble(Console.ReadLine());
+
+double distancia = velocidad * tiempo;
+
+Console.WriteLine("La distancia total recorrida es: " + distancia + " km");
