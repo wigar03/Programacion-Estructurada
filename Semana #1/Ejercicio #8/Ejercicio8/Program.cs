abb8771af0956bb5863fb254775b40e98cb011cc
William <wgarciag@uamv.edu.ni>
2025-08-29 22:19:07 -0600

    Ejercicio #8

---
+++ Semana #1/Ejercicio #8/Ejercicio8/Program.cs
@@ -0,0 +1,17 @@
+﻿int calificacion = 85;
+Console.WriteLine("Proyecto de Notas");
+Console.WriteLine("Por favor ingresa tu calificación");
+calificacion = Convert.ToInt32(Console.ReadLine());
+
+if (calificacion >= 70)
+{
+    Console.WriteLine("Aprobaste el curso");
+}
+else if (calificacion >= 50 && calificacion <=69)
+{
+    Console.WriteLine("Iras a recuperación");
+}
+else if (calificacion <= 50)
+{
+    Console.WriteLine("Reprobaste el curso");
+}
