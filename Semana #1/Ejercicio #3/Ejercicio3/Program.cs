eb825db1bf7d76d9e439548cb0df51ec2963958d
William <wgarciag@uamv.edu.ni>
2025-08-29 22:08:47 -0600

    Ejercicio #3

---
+++ Semana #1/Ejercicio #3/Ejercicio3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Ingrese la velocidad promedio en km/h: ");
+double velocidad = Convert.ToDouble(Console.ReadLine());
+
+// Distancia fija de 40 km
+double distancia = 40;
+
+// Cálculo del tiempo (tiempo = distancia / velocidad)
+double tiempo = distancia / velocidad;
+
+Console.WriteLine("El tiempo total del viaje es: " + tiempo + " horas");
